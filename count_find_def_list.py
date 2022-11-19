1397f391a9c1a456e771912bd7ef6ee39617b024
HEMASE-6566 <114760009+HEMASE-6566@users.noreply.github.com>
2022-11-19 13:25:31 +0330

    Update count_find_def_list.py

--- count_find_def_list.py
+++ count_find_def_list.py
@@ -1,15 +1,15 @@
-def cf (main = str , serch = str):
-    Count = main.count(serch)
+def cf (main = str , search = str):
+    Count = main.count(search)
     print("Count = ",Count)
     start = 0
     L= []
     for i in range(Count):
-        s = main.find(serch,start)
+        s = main.find(search,start)
         L.append(s+1)
         start = s+1
     print("place = ",L)
 #______________________________________________________________________________
 Main = str(input("enter main string : "))
-Serch = str(input("enter serch string : "))
+Search = str(input("enter search string : "))
 #______________________________________________________________________________
-cf(Main , Serch)
+cf(Main , Search)
