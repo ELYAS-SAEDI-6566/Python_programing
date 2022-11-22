9868f8bbf7b5849db5bd33a7964c4b2a89583975
HEMASE-6566 <114760009+HEMASE-6566@users.noreply.github.com>
2022-11-22 20:45:37 +0330

    modified:   simple_codes/make_list.py

--- simple_codes/make_list.py
+++ simple_codes/make_list.py
@@ -2,9 +2,8 @@ def type_int():
     import random
     L1 = []
     L2 = []
-    for i in range(6):
-        if i < 3:
+    for i in range(3):
             L1.append(random.randrange(100))
-        else:
+    for i in range(3):
             L2 = L2 + L1
     return L2
