421e3d918fd587d236af8073f1c2d030ed810586
HEMASE-6566 <114760009+HEMASE-6566@users.noreply.github.com>
2022-11-29 20:45:37 +0330

    new file:   Quera/solution.py 	modified:   Quera/test.py 	new file:   Quera/test2.py

--- Quera/test.py
+++ Quera/test.py
@@ -1,2 +1,6 @@
-for i in 17 :
-    print(i)
+import solution
+print(solution.fruits((
+    {'name':'apple', 'shape': 'sphere', 'mass': 350, 'volume': 120},
+    {'name':'mango', 'shape': 'square', 'mass': 150, 'volume': 120}, 
+    {'name':'lemon', 'shape': 'sphere', 'mass': 300, 'volume': 100},
+    {'name':'apple', 'shape': 'sphere', 'mass': 500, 'volume': 250})))
---
+++ Quera/test2.py
@@ -0,0 +1,3 @@
+x  = {"sss" : 6}
+if "sss" not in x:
+    print("ok")
