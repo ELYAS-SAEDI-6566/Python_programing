2196e073abab6b0ac57e3a0ee61aa13dba53725a
HEMASE-6566 <114760009+HEMASE-6566@users.noreply.github.com>
2022-12-02 21:31:25 +0330

    modified:   Quera/test.py

--- Quera/test.py
+++ Quera/test.py
@@ -1,7 +1,2 @@
-from solution import get_func
-ls = get_func(['square', 'circle', 'rectangle', 'triangle'])
-
-print(ls[0](1))      # 1
-print(ls[1](2))      # 12.566370614359172
-print(ls[2](2, 4))   # 8
-print(ls[3](4, 5))   # 10.0 
+for i in range(1,5):
+    print(i)
