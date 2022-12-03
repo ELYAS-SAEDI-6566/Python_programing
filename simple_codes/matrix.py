fb494094987ec0b688138420cc3d5dca75e60d2e
HEMASE-6566 <114760009+HEMASE-6566@users.noreply.github.com>
2022-12-03 17:02:19 +0330

    Update matrix.py

--- simple_codes/matrix.py
+++ simple_codes/matrix.py
@@ -8,13 +8,13 @@ def create(I = int , J = int):
         matrix.append(row)
     return matrix
 def element_position(matrix , element):
-    List_of_details = []
+    List_of_positions = []
     i = 0 ; j = 0
     for row in matrix:
         j = 0
         for column in row :
             if column == element :
-                List_of_details.append([i,j])
+                List_of_positions.append([i,j])
             j += 1
         i += 1
-    return List_of_details
+    return List_of_positions
