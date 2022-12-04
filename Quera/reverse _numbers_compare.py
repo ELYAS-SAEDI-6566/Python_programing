0abe9fabedaf5fa8b6d069472bc4498b9241ffef
HEMASE-6566 <114760009+HEMASE-6566@users.noreply.github.com>
2022-12-04 21:13:32 +0330

    Update reverse _numbers_compare.py

--- Quera/reverse _numbers_compare.py
+++ Quera/reverse _numbers_compare.py
@@ -3,10 +3,6 @@ num1 = str(input())
 num2 = str(input())
 num1R = num1[::-1]
 num2R = num2[::-1]
-for i in num1L :
-    num1R += i
-for i in num2L :
-    num2R += i
 if num1R > num2R :
     print(f"{num2} < {num1}")
 elif num2R > num1R :
