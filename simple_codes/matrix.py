5f8bb1b3c51414c58f96f7b387f0bd19ec28f661
HEMASE-6566 <114760009+HEMASE-6566@users.noreply.github.com>
2022-12-06 21:16:02 +0330

    modified:   simple_codes/matrix.py

--- simple_codes/matrix.py
+++ simple_codes/matrix.py
@@ -9,12 +9,8 @@ def create(I = int , J = int):
     return matrix
 def element_position(matrix , element):
     List_of_positions = []
-    i = 0 ; j = 0
     for row in matrix:
-        j = 0
         for column in row :
             if column == element :
-                List_of_positions.append([i,j])
-            j += 1
-        i += 1
+                List_of_positions.append([matrix.index(row),row.index(column)])
     return List_of_positions
