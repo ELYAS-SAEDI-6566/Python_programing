343bed90528705fe00436174159b8c4aeefe98c0
HEMASE-6566 <114760009+HEMASE-6566@users.noreply.github.com>
2022-12-23 21:37:10 +0330

    modified:   Quera/test.py 	modified:   Quera/test2.py

--- Quera/test.py
+++ Quera/test.py
@@ -1,5 +1,5 @@
-def x():
-    p = 1
-    return p
-    return p+1
-print(x())
+from khorma import korma2
+for i in range(10) :
+    for j in range(i):
+        print(f"{i} {j}")
+        print(korma2(f"{i} {j}"))
--- Quera/test2.py
+++ Quera/test2.py
@@ -1,2 +1,20 @@
-x = "adadaf"
-print(len(x))
+x = str(input())
+x = x.split()
+s = int(x[0])
+e = int(x[1])
+way = []
+if s == e :
+        print("Saal Noo Mobarak!")
+        exit()
+while(True):
+    if s == e :
+        for i in way:
+            print(i,end="")
+        exit()
+    else:
+        if s > e :
+            s -= 1
+            way.append("L")
+        else :
+            s += 1
+            way.append("R")
