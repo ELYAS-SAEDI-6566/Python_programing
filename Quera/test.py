d653430909dffa16564be2bdad64fa26a7f5f0a6
HEMASE-6566 <114760009+HEMASE-6566@users.noreply.github.com>
2022-12-28 16:10:33 +0330

    modified:   Quera/test.py 	modified:   Quera/test2.py

--- Quera/test.py
+++ Quera/test.py
@@ -1,2 +1,15 @@
-x ="abcd abcd"
-print(x.count("a"))
+x = int(input())
+result = True
+if x == 1:
+    print("fard")
+    exit()
+if x%2 == 0:
+    print("fard")
+else:
+    for i in range(2,x):
+        if x%i == 0:
+            result = False
+    if result :
+        print("zoj")
+    else:
+        print("fard")
--- Quera/test2.py
+++ Quera/test2.py
@@ -1,20 +1,6 @@
-x = str(input())
-x = x.split()
-s = int(x[0])
-e = int(x[1])
-way = []
-if s == e :
-        print("Saal Noo Mobarak!")
-        exit()
-while(True):
-    if s == e :
-        for i in way:
-            print(i,end="")
-        exit()
-    else:
-        if s > e :
-            s -= 1
-            way.append("L")
-        else :
-            s += 1
-            way.append("R")
+# for i in range(0, 10):
+#     print(i)
+x= 15
+for i in range(2,x):
+    if x%i == 0:
+        print(i,x%i)
