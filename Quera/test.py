ff643eec1310d9dafc42f81df48aea7f9b98c4f1
HEMASE-6566 <114760009+HEMASE-6566@users.noreply.github.com>
2023-02-05 03:31:18 +0330

    modified:   Quera/test.py

--- Quera/test.py
+++ Quera/test.py
@@ -1,17 +1,21 @@
-t = input()
-t = t.split()
-a = int(t[0])
-b = int(t[1])
-if a == 0:
-    h = 0
-else:
-    h = 12-a
-if b == 0:
-    m = 0
-else:
-    m = 60-b
-if len(str(h)) == 1 :
-    h = "0"+str(h)
-if len(str(m)) == 1 :
-    m = "0"+str(m)
-print(f"{h}:{m}")
+c1 = str(input())
+c2 = str(input())
+# c1 = "O R"
+# c2 = "O G"
+c1 = c1.split()
+c2 = c2.split()
+# print(c1)
+# print(c2)
+if c1[0] == c1[1]:
+    print("YES")
+    exit()
+if c2[0] == c2[1]:
+    print("YES")
+    exit()
+if c1[0] == c2[1]:
+    print("YES")
+    exit()
+if c1[1] == c2[0]:
+    print("YES")
+    exit()
+print("NO")
