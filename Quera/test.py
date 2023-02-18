03c886250e94aedca7372fac385ecf19e2bae905
HEMASE-6566 <114760009+HEMASE-6566@users.noreply.github.com>
2023-02-18 21:23:09 +0330

    modified:   Quera/test.py

--- Quera/test.py
+++ Quera/test.py
@@ -1,11 +1,2 @@
 n = int(input())
-names = []
-for i in range(n) :
-    n = input()
-    names.append(n.split()[0])
-colors = 1
-for i in names :
-    nameCount = names.count(i)
-    if nameCount > colors:
-        colors = nameCount
-print(colors)
+print(f"Hello CodeCup {n}!")
